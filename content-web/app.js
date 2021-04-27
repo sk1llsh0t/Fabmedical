6eb01c10abab97906316860fd8e55464c8321485
Sk1llSh0t <neumannon@hotmail.com>
2021-04-27 19:48:58 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+appInsights.setup("[YOUR APPINSIGHTS KEY]");
+appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://13.82.67.104:3001";
